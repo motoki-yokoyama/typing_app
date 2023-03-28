202092f3ef386b7deced13e5fe8c65a5abd5d44d
motoki-yokoyama <21ee438@haruka.otemon.ac.jp>
2023-03-28 22:29:54 +0900

    問題表示機能の実装

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QuestionController; 
 
 /*
 |--------------------------------------------------------------------------
@@ -14,13 +15,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//最初のLaravelの画面。後々自作のホーム画面に変更する？
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::controller(QuestionController::class)->middleware(['auth'])->group(function(){
+    Route::get('questions/practice','practice')->name('paractice');
+});
+
+
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
